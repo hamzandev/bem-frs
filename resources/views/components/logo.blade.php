b479966f6adc4255e1f241887868dbcde1f83f62
reny <renywahyuni553@gmail.com>
2024-07-13 12:16:38 +0700

    Update Terbaru

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,10 +1,10 @@
 <a href="/admin" class="flex items-center space-x-3 rtl:space-x-reverse">
-    {{-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" /> --}}
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+    <img src="../img/Logo BEM FRS.png" class="h-8" alt="Logo BEM FRS" />
+    {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
         class="size-6">
         <path stroke-linecap="round" stroke-linejoin="round"
             d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
-    </svg>
+    </svg> --}}
 
     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BEM-FRS</span>
 </a>
