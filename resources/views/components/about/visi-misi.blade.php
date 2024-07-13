682cbfa820197840686c3177901f5ed6f4fd4302
reny <renywahyuni553@gmail.com>
2024-07-13 12:36:33 +0700

    Update Website

--- resources/views/components/about/visi-misi.blade.php
+++ resources/views/components/about/visi-misi.blade.php
@@ -35,12 +35,9 @@
             {{-- Card --}}
             <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Noteworthy technology acquisitions 2024
+                    Menjadikan BEM FRS sebagai wadah Kolaborasi Berdasarkan Kultur dan Budaya untuk Mahasiswa FRS di
+                    Bidang Teknologi agar terwujudnya Mahasiswa yang berintergritas, Kritis, dan Inovatif.
                 </h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">
-                    Here are the biggest enterprise
-                    technology acquisitions of 2021 so far, in reverse chronological order.
-                </p>
             </div>
             {{-- Card --}}
             <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
