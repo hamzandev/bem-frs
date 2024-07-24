13fd245a8c8374319282b74a4135054b9ad72f6d
reny <renywahyuni553@gmail.com>
2024-07-24 13:14:20 +0700

    Update: Struktural

--- resources/views/components/about/struktural.blade.php
+++ resources/views/components/about/struktural.blade.php
@@ -3,13 +3,15 @@
     <div class="container mx-auto">
         <header class="space-y-4 text-center">
             <h3 class="text-3xl font-extrabold">Struktural Pengurus</h3>
-            <p class="text-gray-500 text-lg md:text-xl leading-relaxed lg:w-2/3 lg:mx-auto">Lorem ipsum dolor sit amet
-                consectetur
-                adipisicing elit. Illum quasi nesciunt quis cumque minus quaerat tenetur ab ullam officia
-                voluptas voluptatem, harum aperiam iure unde fuga accusantium recusandae? Dolorem, ducimus.</p>
+            <p class="text-gray-500 text-lg md:text-xl leading-relaxed lg:w-2/3 lg:mx-auto text-justify">Struktur
+                organisasi di bawah ini menggambarkan hierarki dan pembagian tanggung jawab dalam organisasi mahasiswa
+                BEM FRS. Posisi Ketua Badan Eksekutif Mahasiswa sebagai pimpinan tertinggi, diikuti oleh Wakil Ketua,
+                dan selanjutnya terdapat beberapa divisi fungsional seperti Sekretaris, Bendahara, Kepala Biro, dan
+                Kepala Bidang untuk menjalankan kegiatan organisasi secara terstruktur.</p>
         </header>
         {{-- Diagram Struktural --}}
-        <div class="my-10 h-[80vh] shadow w-full bg-gray-200 flex items-center justify-center rounded-2xl md:overflow-hidden overflow-x-scroll">
+        <div
+            class="my-10 h-[80vh] shadow w-full bg-gray-200 flex items-center justify-center rounded-2xl md:overflow-hidden overflow-x-scroll">
             <img src="{{ asset('img/struktural.png') }}" alt="strukural-bem" class="md:w-full md:h-full md:object-cover">
         </div>
         {{-- formatur --}}
@@ -17,10 +19,11 @@
             <h4 class="text-2xl px-3 border-s-4 border-s-blue-500 font-bold">Formatur</h4>
             <div class="grid lg:grid-cols-4 gap-8 py-10">
                 @php
-                    $formatur = ["Ketua", "Wakil Ketua", "Sekretaris", "Bendahara"];
+                    $formatur = ['Ketua', 'Wakil Ketua', 'Sekretaris', 'Bendahara'];
                 @endphp
                 @foreach ($formatur as $i => $item)
-                    <x-about.pengurus-card nama="Person {{ $i + 1 }}" jabatan="{{ $item }}"></x-about.pengurus-card>
+                    <x-about.pengurus-card nama="Person {{ $i + 1 }}"
+                        jabatan="{{ $item }}"></x-about.pengurus-card>
                 @endforeach
             </div>
         </div>
@@ -30,10 +33,11 @@
             <h4 class="text-2xl px-3 border-s-4 border-s-blue-500 font-bold">Biro Ekonomi Kreatif</h4>
             <div class="grid lg:grid-cols-4 gap-8 py-10">
                 @php
-                    $formatur = ["Ketua", "Wakil Ketua", "Sekretaris", "Bendahara"];
+                    $formatur = ['Ketua', 'Wakil Ketua', 'Sekretaris', 'Bendahara'];
                 @endphp
                 @foreach ($formatur as $i => $item)
-                    <x-about.pengurus-card nama="Person {{ $i + 1 }}" jabatan="{{ $item }}"></x-about.pengurus-card>
+                    <x-about.pengurus-card nama="Person {{ $i + 1 }}"
+                        jabatan="{{ $item }}"></x-about.pengurus-card>
                 @endforeach
             </div>
         </div>
@@ -43,10 +47,11 @@
             <h4 class="text-2xl px-3 border-s-4 border-s-blue-500 font-bold">Bidang Riset & Teknologi</h4>
             <div class="grid lg:grid-cols-4 gap-8 py-10">
                 @php
-                    $formatur = ["Ketua", "Wakil Ketua", "Sekretaris", "Bendahara"];
+                    $formatur = ['Ketua', 'Wakil Ketua', 'Sekretaris', 'Bendahara'];
                 @endphp
                 @foreach ($formatur as $i => $item)
-                    <x-about.pengurus-card nama="Person {{ $i + 1 }}" jabatan="{{ $item }}"></x-about.pengurus-card>
+                    <x-about.pengurus-card nama="Person {{ $i + 1 }}"
+                        jabatan="{{ $item }}"></x-about.pengurus-card>
                 @endforeach
             </div>
         </div>
